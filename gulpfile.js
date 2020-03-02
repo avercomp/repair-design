14874651b638795ea5b8ab47aa436fbabc4a81bd
avercomp <avercompete@mail.ru>
2020-03-02 21:45:02 +0300

    incude task rename css files & make main.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 const gulp = require('gulp'); 
 const browserSync = require('browser-sync').create();
+const  cssmin = require('gulp-cssmin');
+const  rename = require('gulp-rename');
+
+// Hello
 gulp.task ('hello', function(done) {
     console.log ('Привет Мир!');
     done();
@@ -13,4 +17,12 @@ gulp.task('browser-sync', function() {
         }
     });
     gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+// minify css using gulp.
+gulp.task('default', function () {
+    gulp.src('src/**/*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist'));
 });
