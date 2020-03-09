7a721fe7328c1ea18c25ee5f6a577faed4539d53
avercomp <avercompete@mail.ru>
2020-03-10 02:05:19 +0300

    refact make responsive for header and hero

--- gulpfile.js
+++ gulpfile.js
@@ -31,6 +31,9 @@ function bs() {
 function serveSass() {
     return src("./sass/**/*.sass","./sass/**/*.scss")
         .pipe(sass())
+        .pipe(autoprefixer({
+            cascade: false
+        }))
         .pipe(dest("./css"))
         .pipe(browserSync.stream());
 };
