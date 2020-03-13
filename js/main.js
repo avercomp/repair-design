1f9472f6b2fcf1e7a5a95bfe6f1c8751119f9324
avercomp <avercompete@mail.ru>
2020-03-14 02:14:12 +0300

    add button-up and jquery scrolltop

--- js/main.js
+++ js/main.js
@@ -24,3 +24,26 @@ $(document).ready(function() {
     })
 });
 
+$(document).ready(function(){
+    /**
+     * При прокрутке страницы, показываем или срываем кнопку
+     */
+    $(window).scroll(function () {
+        // Если отступ сверху больше 50px то показываем кнопку "Наверх"
+        if ($(this).scrollTop() > 50) {
+            $('.button-up').fadeIn();
+        } else {
+            $('.button-up').fadeOut();
+        }
+    });
+    
+    /** При нажатии на кнопку мы перемещаемся к началу страницы */
+    $('.button-up').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 1000);
+        return false;
+    });
+    
+});
+
