d739d8dc4fa35cd19426c6b012aa3a566f31b4ba
avercomp <avercompete@mail.ru>
2020-03-13 21:08:51 +0300

    start modal js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DomContentLoaded", function(event) {
+    const modal = document.querySelector('.modal');
+    const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+    console.log(modalBtn);
+    
+});
