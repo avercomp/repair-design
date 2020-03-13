1e653113a5b819743d58871c35321380c10a2f30
avercomp <avercompete@mail.ru>
2020-03-13 23:31:13 +0300

    modal js ready and work

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
-document.addEventListener("DomContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function(event) {
     const modal = document.querySelector('.modal');
     const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+    const closeBtn = document.querySelector('.modal__close');
+    const switchModal = () => {
+        modal.classList.toggle('modal--visible');
+    }
     console.log(modalBtn);
-    
+    modalBtn.forEach(element => {
+        element.addEventListener('click', switchModal);
+    });
+    closeBtn.addEventListener('click', switchModal)
 });
+
